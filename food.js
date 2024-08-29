2d16b11685dbae3d414d7df2e3925d2d06096a2a
The coding Surveyor <143071196+Isaiah200502@users.noreply.github.com>
2024-08-29 21:50:49 +0100

    Add files via upload

---
+++ food.js
@@ -0,0 +1,8 @@
+document.getElementById('contact-form')
+addEventListener('submit',function(event)
+{
+    //Prevent the default form submission
+    event.preventDefault();
+    //Handle form submission, e.g.,sending data to a server
+    //For demonstration purposes, you could add validation and form submission logic here
+})
